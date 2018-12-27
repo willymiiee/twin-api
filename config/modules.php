b46c09ef87eaeed97a35320afc1d3de6a615cb64
Willy Nugraha <willymiiee@gmail.com>
2018-12-27 09:27:35 +0800

    add configs for auth, modules & sluggable

---
+++ config/modules.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'basic' => [
+        'company',
+        'location',
+        'department',
+        'job-title',
+        'role',
+        'user',
+        'profile',
+        'trip',
+        'store',
+        'item',
+    ]
+];
---
+++ config/sluggable.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'source' => null,
+    'maxLength' => null,
+    'maxLengthKeepWords' => true,
+    'method' => null,
+    'separator' => '-',
+    'unique' => true,
+    'uniqueSuffix' => null,
+    'includeTrashed' => false,
+    'reserved' => null,
+    'onUpdate' => false,
+];
