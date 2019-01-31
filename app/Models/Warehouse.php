ae91f6eddb21bf2faa5e5347c076fe69792208ef
Willy Nugraha <willymiiee@gmail.com>
2019-01-31 23:38:45 +0800

    Depot, Warehouse models & migrations

---
+++ app/Models/Warehouse.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Warehouse extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'company_id',
+        'code',
+        'name',
+        'address',
+        'latitude',
+        'longitude',
+        'phone',
+        'province_id',
+        'district_id',
+        'subdistrict_id',
+        'village_id',
+        'zipcode',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function depots()
+    {
+        return $this->hasMany('App\Models\Depots');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo('App\Models\Company');
+    }
+
+    public function province()
+    {
+        return $this->belongsTo('App\Models\Province');
+    }
+
+    public function district()
+    {
+        return $this->belongsTo('App\Models\District');
+    }
+
+    public function subdistrict()
+    {
+        return $this->belongsTo('App\Models\Subdistrict');
+    }
+
+    public function village()
+    {
+        return $this->belongsTo('App\Models\Village');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo('App\Models\User', 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo('App\Models\User', 'updated_by');
+    }
+
+    public function deleter()
+    {
+        return $this->belongsTo('App\Models\User', 'deleted_by');
+    }
+}
---
+++ database/migrations/2019_01_31_182141_create_depots_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('depots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned();
+            $table->integer('warehouse_id')->unsigned();
+            $table->string('name');
+            $table->text('address')->nullable();
+            $table->text('latitude')->nullable();
+            $table->text('longitude')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('province_id', 2)->nullable();
+            $table->string('district_id', 4)->nullable();
+            $table->string('subdistrict_id', 7)->nullable();
+            $table->string('village_id', 10)->nullable();
+            $table->integer('zipcode')->unsigned()->nullable();
+            $table->integer('created_by')->unsigned();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->integer('deleted_by')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('depots');
+    }
+}
