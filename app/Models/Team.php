59600d742a2526cfabb2c127ced14c32a175a321
Willy Nugraha <willymiiee@gmail.com>
2019-02-01 22:01:49 +0800

    Team model & migration

---
+++ app/Models/Team.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Team extends Model
+{
+    use SoftDeletes;
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    public function salesmen()
+    {
+        return $this->belongsToMany('App\Models\User', 'user_team');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo('App\Models\Company');
+    }
+
+    public function depot()
+    {
+        return $this->belongsTo('App\Models\Depot');
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo('App\Models\User', 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo('App\Models\User', 'updated_by');
+    }
+
+    public function deleter()
+    {
+        return $this->belongsTo('App\Models\User', 'deleted_by');
+    }
+}
---
+++ database/migrations/2019_02_01_190308_create_teams_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned();
+            $table->integer('depot_id')->unsigned();
+            $table->string('name');
+            $table->integer('created_by')->unsigned();
+            $table->integer('updated_by')->unsigned()->nullable();
+            $table->integer('deleted_by')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
